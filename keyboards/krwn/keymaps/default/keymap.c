35fbf05846f2bfec5e05e25fa93060fd4faf6d15
codybender <codyfbender@gmail.com>
2019-07-11 16:16:13 -0600

    added all initial files for KRWN

--- keyboards/krwn/keymaps/default/keymap.c
+++ keyboards/krwn/keymaps/default/keymap.c
@@ -1,9 +1,23 @@
+/* Copyright 2019 Kilgarah
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include QMK_KEYBOARD_H
 
-#define _QWERTY 0
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT_krwn(
+  [0] = LAYOUT_krwn(
     KC_F1,  KC_F2,  KC_ESC,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS,  KC_EQL,  KC_BSLS,  KC_GRV,  KC_INS,
     KC_F3,  KC_F4,  KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC,  KC_RBRC,  KC_BSPC, KC_DEL,
     KC_F5,  KC_F6,  KC_CLCK,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,  KC_ENT,  KC_PGUP,
@@ -11,3 +25,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F9,  KC_F10, KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 };
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
