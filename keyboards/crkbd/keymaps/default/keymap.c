39563e5ebc8c57740f75202918663cc8c5bb44ec
Cody Bender <50554676+cfbender@users.noreply.github.com>
2019-09-03 10:19:16 -0600

    Update keymap.c

--- keyboards/crkbd/keymaps/default/keymap.c
+++ keyboards/crkbd/keymaps/default/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-------+-------+-------+-------+-------+-------|                   |-------+-------+-------+-------+-------+-------|
      OPT_TAB, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                     KC_H  , KC_J  , KC_K  ,  KC_L ,KC_SCLN,KC_QUOT,\
   //|-------+-------+-------+-------+-------+-------|                   |-------+-------+-------+-------+-------+-------|
-     KC_LSFT,KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  ,                     KC_B  , KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,\
+     KC_LSFT,KC_LALT, KC_Z  , KC_X  , KC_C  , KC_V  ,                     KC_B  , KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,\
   //|-------+-------+-------+-------+-------+-------+-------|   |-------+-------+-------+-------+-------+-------+-------|
                                       LOWER ,KC_LGUI,KC_SPC,    KC_SPC , KC_ENT, RAISE \
                                    //|------+-------+-------|   |-------+-------+-------|
